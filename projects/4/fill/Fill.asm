a3d01f66a6a95500ca3274308037265775c71389
harshith276 <harshith918273@gmail.com>
2025-05-11 21:07:08 +0530

    Updated Projects

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,59 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Fill.asm
+// Fills or clears the screen based on keyboard input
+
+// Infinite loop
+(LOOP)
+    @KBD
+    D=M
+    @BLACK
+    D;JNE       // If a key is pressed, go to BLACK
+    @WHITE
+    0;JMP       // Else, go to WHITE
+
+// Fill screen with black (-1)
+(BLACK)
+    @SCREEN
+    D=A
+    @addr
+    M=D         // addr = SCREEN base address
+
+    @fillValue
+    M=-1        // fillValue = -1 (black)
+    @FILL
+    0;JMP
+
+// Fill screen with white (0)
+(WHITE)
+    @SCREEN
+    D=A
+    @addr
+    M=D         // addr = SCREEN base address
+
+    @fillValue
+    M=0         // fillValue = 0 (white)
+    @FILL
+    0;JMP
+
+// Fill routine
+(FILL)
+    @fillValue
+    D=M         // D = value to write
+    @addr
+    A=M         // A = current screen address
+    M=D         // Write D to screen
+
+    @addr
+    M=M+1       // addr++
+
+    @KBD
+    D=A
+    @addr
+    D=D-M       // D = KBD - addr
+
+    @FILL
+    D;JGT       // If addr < KBD, keep going
+    @LOOP
+    0;JMP       // Else, check keyboard again
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Mult.asm
+// Multiplies R0 and R1 using repetitive addition and stores result in R2
+
+// Initialize result to 0
+@0
+D=M         // D = R0
+@counter
+M=D         // counter = R0
+
+@2
+M=0         // R2 = 0 (clear result)
+
+// If counter == 0, skip loop
+@counter
+D=M
+@END
+D;JEQ
+
+// Load R1 into temp
+@1
+D=M
+@temp
+M=D         // temp = R1
+
+(LOOP)
+    // Add temp (R1) to R2
+    @temp
+    D=M
+    @2
+    M=M+D     // R2 += temp
+
+    // counter--
+    @counter
+    M=M-1
+
+    // If counter > 0, repeat
+    @counter
+    D=M
+    @LOOP
+    D;JGT
+
+(END)
+    // Infinite loop to end program
+    @END
+    0;JMP
